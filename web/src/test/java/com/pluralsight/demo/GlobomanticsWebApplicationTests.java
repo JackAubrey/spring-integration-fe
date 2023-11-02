f03da69793862a2e9fafb3b8932ae94290c6ce3d
Danilo Cividin <danilo.cividin.ext@infocert.it>
2023-11-02 17:00:37 +0100

    first commit. updated project to spring 3.1.5 and JDK 21

---
+++ web/src/test/java/com/pluralsight/demo/GlobomanticsWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pluralsight.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class GlobomanticsWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
